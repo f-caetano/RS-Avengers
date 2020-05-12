737c30cfb94d9424cf01000115659c66be4d0a1f
v-ficaet <61557698+v-ficaet@users.noreply.github.com>
2020-05-12 14:37:20 +0100

    Collects Windows Events (App, System and Setup)

--- EventViewer/WindowsEvents.ps1
+++ EventViewer/WindowsEvents.ps1
@@ -1,9 +1,9 @@
-﻿<# Requires .NET Framework 3.5+
+<# Requires .NET Framework 3.5+
 Collects Windows Event Viewer logs into a ZIP file
 After runing, please remove the temporary created folder: "_WindowsEvents"
 ___________________________________________________________________________#>
 
-# Runtime Variables
+# 1.Runtime Variables
 $Date = Get-Date
 $MachineName = $env:COMPUTERNAME.ToLower()
 $MonthDay = $Date.ToString("MMdd")
@@ -15,26 +15,26 @@ $ZipFile = "$RuningDir\WindowsEvents_"+$MonthDay+"_("+$MachineName+").zip"
 $WindowsEvents = New-Object System.Diagnostics.Eventing.Reader.EventLogSession
 $query = "*[System[TimeCreated[timediff(@SystemTime) < 604800000]]]"
 
-# Create temporary working folder "_WindowsEvents"
+# 2.Create temporary working folder "_WindowsEvents"
 Write-Host "Running... `n" -NoNewline -ForegroundColor Black -BackgroundColor white
 if(!(Test-Path $SavingDir)) {New-Item -ItemType Directory -Force -Path $SavingDir | Out-Null}
 ELSE 
 {Remove-item "$SavingDir\*" -Include *.evtx}
 
 
-# Extract Windows Events
-Write-output "Collecting: System event logs"
+# 3.Extract Windows Events
+Write-Host "Collecting: Windows System Events"
 $WindowsEvents.ExportLog("System","LogName",$query,$EventFile+"SYSTEM.evtx")
-Write-Host "Collecting: Application event logs"
+Write-Host "Collecting: Windows Application Events"
 $WindowsEvents.ExportLog("Application","LogName",$query,$EventFile+"APPLICATION.evtx")
-<# requires elevated/admin run
-Write-Host "Collecting: Security event logs"
+Write-Host "Collecting: Windows Setup Events"
+$WindowsEvents.ExportLog("Setup","LogName",$query,$EventFile+"SETUP.evtx")
+<# requires elevated admin run
+Write-Host "Collecting: Windows Security Events"
 $WindowsEvents.ExportLog("Security","LogName",$query,$EventFile+"SECURITY.evtx")
 #>
-Write-Host "Collecting: Setup event logs"
-$WindowsEvents.ExportLog("Setup","LogName",$query,$EventFile+"SETUP.evtx")
 
-# .ZIP the files
+# 4.ZIP the files
 if ($error.Count -eq 0)
 {
 if (Test-Path $ZipFile) {Remove-item $ZipFile}
@@ -45,6 +45,6 @@ Write-Host "DONE       `n" -NoNewline -ForegroundColor Black -BackgroundColor Gr
 Write-Host "Please share the generated file bellow:`n $ZipFile"
 ii $RuningDir
 }
-# Comment if run on PS ISE
+# 5.Comment if run on PS ISE
 [void][System.Console]::ReadKey($true)
 
