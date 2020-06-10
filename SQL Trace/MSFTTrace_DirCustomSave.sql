33d8bd17fb7e12a2a4205f953d14fd4fac12bee9
v-ficaet <61557698+v-ficaet@users.noreply.github.com>
2020-06-10 15:43:38 +0100

    SQL Server xEvent Trace (Auto)
    Added file amount/size limit

--- SQL Trace/MSFTTrace_DirCustomSave.sql
+++ SQL Trace/MSFTTrace_DirCustomSave.sql
@@ -34,7 +34,7 @@ ADD EVENT sqlserver.sql_statement_completed(SET collect_statement=(1)
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id)),
 ADD EVENT sqlserver.sql_statement_starting(
     ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id))
-ADD TARGET package0.event_file(SET filename=N'''+ @DIR + '_MSFTTrace.xel'')
+ADD TARGET package0.event_file(SET filename=N'''+ @DIR + '_MSFTTrace.xel'',max_file_size=(2048),max_rollover_files=(5))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=3 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)'
 EXECUTE (@sql)
 GO
