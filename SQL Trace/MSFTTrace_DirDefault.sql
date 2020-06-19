a9fb252754531f288541db8541d50f761aa53402
v-ficaet <61557698+v-ficaet@users.noreply.github.com>
2020-06-19 21:56:45 +0100

    SQL Server xEvent Trace (Manual)
    +Added RPC
    +Minimizing the collected data by removing some started events 
    +Filtering wait_info and attention

--- SQL Trace/MSFTTrace_DirDefault.sql
+++ SQL Trace/MSFTTrace_DirDefault.sql
@@ -15,21 +15,23 @@ GO
 /** 2. Create the Extended Events Trace **/
 CREATE EVENT SESSION [_MSFTTrace] ON SERVER 
 ADD EVENT sqlos.wait_info(
-    ACTION(sqlserver.session_id)),
+    ACTION(sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
+    WHERE ([opcode]=(1) AND [duration]>(1000))),
 ADD EVENT sqlserver.attention(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)),
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)
+    WHERE ([sqlserver].[is_system]=(0))),
 ADD EVENT sqlserver.blocked_process_report(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)),
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
 ADD EVENT sqlserver.error_reported(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.session_id,sqlserver.sql_text)),
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
+ADD EVENT sqlserver.rpc_completed(
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
+ADD EVENT sqlserver.rpc_starting(
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
 ADD EVENT sqlserver.sp_statement_completed(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)),
-ADD EVENT sqlserver.sp_statement_starting(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)),
-ADD EVENT sqlserver.sql_statement_completed(SET collect_statement=(1)
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id)),
-ADD EVENT sqlserver.sql_statement_starting(
-    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id))
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text,sqlserver.username)),
+ADD EVENT sqlserver.sql_statement_completed(
+    ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_name,sqlserver.session_id,sqlserver.username))
     ADD TARGET package0.event_file(SET filename=N'_MSFTTrace.xel',max_file_size=(2048),max_rollover_files=(5))
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=3 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
  
