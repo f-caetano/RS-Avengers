5f2216c1c8378c45fad3220daef6c8f59ee62956
v-ficaet <61557698+v-ficaet@users.noreply.github.com>
2020-06-21 00:21:42 +0100

    SQL Server xEvent Trace (Manual)
    Changed some tooltips and steps information

--- SQL Trace/MSFTTrace_DirDefault.sql
+++ SQL Trace/MSFTTrace_DirDefault.sql
@@ -1,18 +1,18 @@
-/*=================== README ======================================================================
+/*=================== README ============================================================================
 Running the entire script will start the trace. SysAdmin permission on the SQL Server is required
 
-After step 3: Code is comment with "--" and the T-SQL needs to be executed manually
-Please follow the information and select only the specific lines to avoid running the entire script
+After step 3: Code is needed to be executed manually. Further information details are on the steps 
+              Please select only the specific code lines of the step to avoid running the entire script
+=========================================================================================================*/
 
-===================================================================================================*/
 USE master;
 GO
-/** 1. Cleanup **/
+/** 1. Cleanup                                      **/
 IF EXISTS(SELECT * FROM sys.server_event_sessions WHERE name='_MSFTTrace')  
     DROP EVENT SESSION [_MSFTTrace] ON SERVER;  
 GO
  
-/** 2. Create the Extended Events Trace **/
+/** 2. Create the Extended Events Trace             **/
 CREATE EVENT SESSION [_MSFTTrace] ON SERVER 
 ADD EVENT sqlos.wait_info(
     ACTION(sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
@@ -37,22 +37,28 @@ ADD EVENT sqlserver.sql_statement_completed(
 WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=3 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
  
  
-/** 3. Start the xEvent Trace **/
+/** 3. Start the xEvent Trace                      **/
 ALTER EVENT SESSION [_MSFTTrace] ON SERVER  
 STATE = START;
  
-/** 4. Stop the xEvent Trace  **/
---ALTER EVENT SESSION [_MSFTTrace] ON SERVER  
---STATE = STOP;
+/** 4. Stop the xEvent Trace                       **/
+ALTER EVENT SESSION [_MSFTTrace] ON SERVER  
+STATE = STOP;
+                                                   **/
+/** 5. Export the generated xEvent (.xel) file     **
+
+Two options
+1) Inside the SQL Server machine locate the file in the SQL Directory:
+   usually: "<SQL Server Installation Path>\Microsoft SQL Server\MSSQLxx.MSSQLSERVER\Log\file.xel"
  
-/** 5. SSMS: Export the xEvent to a file on your local disk 
- 
-ON SSMS Object Explorer (left Panel) Expand folders: Management» Extended Events» Sessions» _MSFTrace
-Double click 'package0.event_file' will open the collected events in a table format
-On the Menu toolbar for SSMS go to: Extended Events» Export To» XEL File...
-Save this file on a directory at your choice 
- 
+2) Using SSMS, and saving the trace on a client workstation
+    a) On the SSMS Object Explorer (our left Panel) expand the folder 'Management' then Extended Events» Sessions» _MSFTrace
+    b) Under the name of the trace, double click 'package0.event_file' and it will open the collected events in a table format
+    c) Wait a few seconds for them to load, then on the top bar of SSMS (Menu toolbar) select: Extended Events» Export To» XEL File...
+    d) Save this file on a directory at your choice 
 **/
  
-/** 6. With the file saved (.xel) remove the xEvent from your SQL Server  **/
---DROP EVENT SESSION [_MSFTTrace] ON SERVER;
+/** 6. With the file saved (.xel) remove the xEvent created from the SQL Server  **/
+DROP EVENT SESSION [_MSFTTrace] ON SERVER;
+                                                                                 **/
+/** 7. All done. Share the xEvent file (.xel)                                    **/
