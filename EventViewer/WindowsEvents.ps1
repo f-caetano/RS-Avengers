daef334c50152cc2723884552cccd9dd52bc13af
Filipe Caetano <61557698+f-caetano@users.noreply.github.com>
2024-02-02 12:23:35 +0000

    Refactor WindowsEvents.ps1
    Added days parameter and ability to choose to export security events

--- EventViewer/WindowsEvents.ps1
+++ EventViewer/WindowsEvents.ps1
@@ -1,50 +1,58 @@
 <# Requires .NET Framework 3.5+
 Collects Windows Event Viewer logs into a ZIP file
-After runing, please remove the temporary created folder: "_WindowsEvents"
+Please review contents before sharing and remove the temporary created folder: "_WindowsEvents"
 ___________________________________________________________________________#>
+# 1.Customizable Variables
+$DaysRange = 7
+$IncludeSecurityEvent = 'N' # Y = Requires elevated administrator run
 
-# 1.Runtime Variables
-$Date = Get-Date
+# 2.Runtime Variables
 $MachineName = $env:COMPUTERNAME.ToLower()
-$MonthDay = $Date.ToString("MMdd")
+$DateFormat = (Get-Date).ToString("yyyy-MM-dd")
 $RuningDir = $PSScriptRoot
 $SavingDir = "$RuningDir\_WindowsEvents"
-$EventFile = "$SavingDir\Event_"+$MonthDay+"_("+$MachineName+")_"
-$ZipFile = "$RuningDir\WindowsEvents_"+$MonthDay+"_("+$MachineName+").zip"
+$EventFile = "$SavingDir\"+$DateFormat+"_("+$MachineName+")_"
+$ZipFile = "$RuningDir\WindowsEvents_"+$DateFormat+"_("+$MachineName+").zip"
 
 $WindowsEvents = New-Object System.Diagnostics.Eventing.Reader.EventLogSession
-$query = "*[System[TimeCreated[timediff(@SystemTime) < 604800000]]]"
-
-# 2.Create temporary working folder "_WindowsEvents"
-Write-Host "Running... `n" -NoNewline -ForegroundColor Black -BackgroundColor white
-if(!(Test-Path $SavingDir)) {New-Item -ItemType Directory -Force -Path $SavingDir | Out-Null}
-ELSE 
-{Remove-item "$SavingDir\*" -Include *.evtx}
-
-
-# 3.Extract Windows Events
-Write-Host "Collecting: Windows System Events"
-$WindowsEvents.ExportLog("System","LogName",$query,$EventFile+"SYSTEM.evtx")
-Write-Host "Collecting: Windows Application Events"
-$WindowsEvents.ExportLog("Application","LogName",$query,$EventFile+"APPLICATION.evtx")
-Write-Host "Collecting: Windows Setup Events"
-$WindowsEvents.ExportLog("Setup","LogName",$query,$EventFile+"SETUP.evtx")
-<# requires elevated admin run
-Write-Host "Collecting: Windows Security Events"
-$WindowsEvents.ExportLog("Security","LogName",$query,$EventFile+"SECURITY.evtx")
-#>
-
-# 4.ZIP the files
-if ($error.Count -eq 0)
-{
-if (Test-Path $ZipFile) {Remove-item $ZipFile}
-Add-Type -assembly "system.io.compression.filesystem" 
-[io.compression.zipfile]::CreateFromDirectory($SavingDir,$ZipFile)
-
-Write-Host "DONE       `n" -NoNewline -ForegroundColor Black -BackgroundColor Green
-Write-Host "Please share the generated file bellow:`n $ZipFile"
-ii $RuningDir
+$query = "*[System[TimeCreated[timediff(@SystemTime) < $($daysRange * 24 * 60 * 60 * 1000)]]]"
+$params = @{LogName = $query; Session = $WindowsEvents}
+if ($IncludeSecurityEvent -ne 'N') {
+    $logNames = "System", "Application", "Setup", "Security"
 }
-# 5.Comment if run on PS ISE
-[void][System.Console]::ReadKey($true)
+else {
+    $logNames = "System", "Application", "Setup"
+}
+
+# 3.Validate no previous export and create temporary working folder "_WindowsEvents"
+$evtxFiles = Get-ChildItem -Path $SavingDir -Filter *.evtx -Recurse | Measure-Object
+if ($evtxFiles.Count -gt 0) {
+   Write-Error "Folder '$SavingDir' contains .EVTX files. Please delete or move them before running the script."
+   Pause
+   exit
+}
+if(!(Test-Path $SavingDir)) {New-Item -ItemType Directory -Force -Path $SavingDir | Out-Null}
+Write-Host "Collecting ${DaysRange} days:         `n" -NoNewline -ForegroundColor Black -BackgroundColor white
+
+# 4.Extract Windows Events
+try {
+    foreach ($logName in $logNames) {
+        Write-Output "Windows $logName Events"
+        $WindowsEvents.ExportLog($logName, "LogName", $query, $EventFile+"$logName.evtx", $true)
+    }
+}
+catch {
+    Write-Error $_.Exception.Message
+    Pause
+}
+
+# 5.Group files into .ZIP
+If (Test-Path $ZipFile) {Remove-item $ZipFile}
+    Add-Type -assembly "system.io.compression.filesystem" 
+    [io.compression.zipfile]::CreateFromDirectory($SavingDir,$ZipFile)
+    Write-Host "Finished                   `n" -NoNewline -ForegroundColor Black -BackgroundColor Green
+
+# End
+Read-Host -Prompt "`nPress any key to exit and to open the directory:`n $ZipFile"
+ii $RuningDir
 
